38791ba589fe81cfc2e5e54c86ad8be81d989962
kgaikj <kgaikj2cu@icloud.com>
2023-08-03 11:36:17 +0800

    chore: add tailwindcss & styled & motion to deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+
 import "./App.css";
+import "./styles.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
